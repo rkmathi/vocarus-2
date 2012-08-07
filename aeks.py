e24e997012e068f7b336b51c7f0be04f22eb83c4
Ryuichi KAWAMATA (rkmathi) <mail@rkmathi.net>
2012-08-07 14:31:33 +0900

    Mod aeks.py => ALPHA support for minor song

--- aeks.py
+++ aeks.py
@@ -3,9 +3,9 @@
 import sys
 import copy
 
-def pentatonic(code, note_list):
+def majorpentatonic(code, note_list):
     note_names = {'c':0, 'cis':1, 'd':2, 'dis':3, 'e':4, 'f':5,
-                  'fis':6, 'g':7, 'gis':8, 'a':9, 'b':10, 'h':11}
+                 'fis':6, 'g':7, 'gis':8, 'a':9, 'b':10, 'h':11}
     inter2 = [2, 2, 3, 2, 0]
     count = 0
     for i in range(0, len(note_list)):
@@ -16,10 +16,21 @@ def pentatonic(code, note_list):
             tmp += inter2[k]
     return count
 
+def minorpentatonic(code, note_list):
+    note_names = {'c':0, 'cis':1, 'd':2, 'dis':3, 'e':4, 'f':5,
+                 'fis':6, 'g':7, 'gis':8, 'a':9, 'b':10, 'h':11}
+    inter2 = [3, 2, 2, 4, 0]
+    count = 0
+    for i in range(0, len(note_list)):
+        tmp = code
+        for k in range(0, 5):
+            if note_names[note_list[i][0]] == tmp%12:
+                count += 1
+            tmp += inter2[k]
+    return count
 
 def aeks(notes):
-    """Automatic Estimation of Key Signature(AEKS)
-
+    """Automatic Estimation of Key Signature
     Args:
         notes: Soprano notes (IntegerList)
     Returns:
@@ -27,20 +38,21 @@ def aeks(notes):
     """
 
     # C<-2->D, D<-2->E, E<-1->F, F<-2->G, G<-2->A, A<-2->H
-    inter = [2, 2, 1, 2, 2, 2, 1]
+    majorinter = [2, 2, 1, 2, 2, 2, 1]
+    minorinter = [2, 1, 2, 2, 1, 3, 1]
 
     # C->0, Cis->1, D->2, Dis->3, E->4, F->5,
     # Fis->6, G->7, Gis->8, A->9, B->10, H->11
     note_name  = ['c', 'cis', 'd', 'dis', 'e', 'f',
                   'fis', 'g', 'gis', 'a', 'b', 'h']
     note_names = {'c':0, 'cis':1, 'd':2, 'dis':3, 'e':4, 'f':5,
-                  'fis':6, 'g':7, 'gis':8, 'a':9, 'b':10, 'h':11}
+                 'fis':6, 'g':7, 'gis':8, 'a':9, 'b':10, 'h':11}
     note_occur = {'c':0, 'cis':0, 'd':0, 'dis':0, 'e':0, 'f':0,
                   'fis':0, 'g':0, 'gis':0, 'a':0, 'b':0, 'h':0}
 
     # Count times that notes appear.
     for n in notes:
-        note_occur[note_name[(n)%12]] += 1
+        note_occur[note_name[(n)%12]] += 1 # +7
 
     # Make dictionary sorted.
     note_sorted = sorted(note_occur.items(),
@@ -55,13 +67,14 @@ def aeks(notes):
         else:
             pass
 
-# !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! #
+# !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! #
     #print "note2_list==>",note2_list,"<br/>"
     #for i in range(0, 7):
     #    print "note_names[note2_list[",i,"][0]]",note_names[note2_list[i][0]],"<br />"
-# !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! #
+# !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! !! #
 
     max = [-1, -1]
+
     for i in range(0, len(note2_list)):
         count = 0
         for t in range(0, len(note2_list)):
@@ -69,13 +82,29 @@ def aeks(notes):
             for j in range(0, 7):
                 if note_names[note2_list[t][0]] == (code%12):
                     count += 1
-                code += inter[j]
+                code += majorinter[j]
         if count == len(note2_list):
-            penta = pentatonic(note_names[note2_list[i][0]], note2_list)
+            penta = majorpentatonic(note_names[note2_list[i][0]], note2_list)
             if max[1] < penta:
                 max[0] = note_names[note2_list[i][0]]
                 max[1] = penta
 
+    if max[0] == -1:
+        for i in range(0, len(note2_list)):
+            count = 0
+            for t in range(0, len(note2_list)):
+                code = note_names[note2_list[i][0]]
+                for j in range(0, 7):
+                    if note_names[note2_list[t][0]] == (code%12):
+                        count += 1
+                    code += minorinter[j]
+            if count == len(note2_list):
+                penta = minorpentatonic(note_names[note2_list[i][0]], note2_list)
+                if max[1] < penta:
+                    max[0] = note_names[note2_list[i][0]]
+                    max[1] = penta
+        key = max[0] + 1
+        return -key      
+
     key = max[0] + 1
     return key
-
